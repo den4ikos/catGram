12f040abcc41692a20edcf053eb5165795504612
Denis <dev@webernetic.by>
2022-10-01 13:39:41 +0300

    add UserService

--- src/main/java/ru/yandex/practicum/catsgram/service/PostService.java
+++ src/main/java/ru/yandex/practicum/catsgram/service/PostService.java
@@ -1,6 +1,8 @@
 package ru.yandex.practicum.catsgram.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import ru.yandex.practicum.catsgram.exception.UserNotFoundException;
 import ru.yandex.practicum.catsgram.model.Post;
 
 import java.util.ArrayList;
@@ -9,12 +11,21 @@ import java.util.List;
 @Service
 public class PostService {
     private final List<Post> posts = new ArrayList<>();
+    private final UserService userService;
+
+    @Autowired
+    public PostService(UserService userService) {
+        this.userService = userService;
+    }
 
     public List<Post> findAll() {
         return posts;
     }
 
     public Post create(Post post) {
+        if (!userService.findUserByEmail(post.getAuthor())) {
+            throw new UserNotFoundException("Пользователь " + post.getAuthor() + " не найден");
+        }
         posts.add(post);
         return post;
     }
